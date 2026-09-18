fadc6535fbfbb2b4b575924e9f85dc269ab9dec7
Zara18Wheels <Zahra.Ghasemifard@18wheels.ca>
2026-09-17 20:40:16 -0700

    Add FileReader to read Excel file as ArrayBuffer

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ let fileInput = document.getElementById("excelFile");
 fileInput.addEventListener("change", function() {
     let file = fileInput.files[0];
     let reader = new FileReader();
+    reader.readAsArrayBuffer(file);
 });
