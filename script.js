430fd7becc4de559be3d22fd38686c47818790c2
Zara18Wheels <Zahra.Ghasemifard@18wheels.ca>
2026-09-17 20:35:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let fileInput = document.getElementById("excelFile");
 fileInput.addEventListener("change", function() {
     let file = fileInput.files[0];
-    console.log(file);
+    let reader = new FileReader();
 });
